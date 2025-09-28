99fd9e890f026a57e48889d5f398a48ce707f8fb
Diego <diegosantos4957@gmail.com>
2025-09-28 20:47:16 -0300

    feat: Update Home component to improve product rendering and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import ApiRequest from "@/requests/api";
 
 export default async function Home() {
-    const products = await ApiRequest('/products', {
+    const products = await ApiRequest<{id: string, name: string}[]>('/products', {
         cache: 'force-cache',
         next: { revalidate: 600 },
         headers: {
@@ -15,10 +15,19 @@ export default async function Home() {
 
     return (
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            <div>
+           <main>
+               {products?.map((product) =>
+                   <div key={product.id}>
+                     <Image src={`https://dummyimage.com/600x400/cccccc/000000&text=`} alt={`Product`} width={300} height={200} />
+                     <p>{product.name}</p>
+                   </div>
+               )}
+           </main>
+
+            {/*<div>
                 <Image src='https://dummyimage.com/600x400/cccccc/000000&text=' alt={""} width={300} height={300} />
                 <>{JSON.stringify(products, null, 2) }</>
-            </div>
+            </div>*/}
         </div>
     );
 }
