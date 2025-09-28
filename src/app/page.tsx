f80ae68f21c541f3f5620da38175c8f3d18a5c53
Diego <diegosantos4957@gmail.com>
2025-09-28 12:53:06 -0300

    feat: Update image handling in Home component to use static URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,11 @@ export default async function Home() {
     const response = await fetch('http://localhost:8079/products')
     const products = await response.json();
 
-    const responseImages = await fetch('https://dummyimage.com/600x400/cccccc/000000&text=')
-    const images = await responseImages.json();
-
 
     return (
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
             <div>
-                <Image src={images} alt={""} />
+                <Image src='https://dummyimage.com/600x400/cccccc/000000&text=' alt={""} width={300} height={300} />
                 <>{JSON.stringify(products, null, 2) }</>
             </div>
         </div>
