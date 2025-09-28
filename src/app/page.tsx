f602e54085003401d84c4ed022ec7ae28b8e4066
Diego <diegosantos4957@gmail.com>
2025-09-28 20:53:57 -0300

    feat: Add console log for products in page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,11 @@ export default async function Home() {
         },
     })
 
+    console.log({products});
+
 
     return (
-        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <div className="">
            <main>
                {products?.map((product) =>
                    <div key={product.id}>
