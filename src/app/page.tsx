c0dc0737c0e25986f3034b330855aa43850eeddc
Diego <diegosantos4957@gmail.com>
2025-09-28 12:47:02 -0300

    feat: Update Home component to fetch and display product images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 'use server';
 
-export default async function Home() {
-    const products = await fetch('http://localhost:8079/products')
-    console.log({products: products.json()})
+import Image from "next/image";
 
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        <div>
-            <>{JSON.stringify(products, null, 2) }</>
+export default async function Home() {
+    const response = await fetch('http://localhost:8079/products')
+    const products = await response.json();
+
+    const responseImages = await fetch('https://dummyimage.com/600x400/cccccc/000000&text=')
+    const images = await responseImages.json();
+
+
+    return (
+        <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <div>
+                <Image src={images} alt={""} />
+                <>{JSON.stringify(products, null, 2) }</>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
