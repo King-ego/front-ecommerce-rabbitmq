1a2fd12f1efe7b9d003f465e924ec742a12e1987
Diego <diegosantos4957@gmail.com>
2025-09-29 00:08:37 -0300

    feat: Update product display to use links for product names and improve layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import ApiRequest from "@/requests/api";
 import Product from "@/requests/interfaces/Product";
+import Link from "next/link";
 
 export default async function Home() {
 	const products = await ApiRequest<Product[]>('/products', {
@@ -17,23 +18,23 @@ export default async function Home() {
 
 
 	return (
-		<div className="">
-			<main>
+		<section className="w-full">
+			<div className="flex flex-wrap p-8 gap-4">
 				{products?.map((product) =>
 					<div key={product.id}>
 						<Image src={`https://dummyimage.com/600x400/cccccc/000000&text=`} alt={`Product`} width={300}
 							   height={200}/>
-						<p>{product.name}</p>
+						<Link href={`/product/${product.id}`}>{product.name}</Link>
 						<p>{product.description}</p>
 						<p>{Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(product.price)}</p>
 					</div>
 				)}
-			</main>
+			</div>
 
 			{/*<div>
                 <Image src='https://dummyimage.com/600x400/cccccc/000000&text=' alt={""} width={300} height={300} />
                 <>{JSON.stringify(products, null, 2) }</>
             </div>*/}
-		</div>
+		</section>
 	);
 }
