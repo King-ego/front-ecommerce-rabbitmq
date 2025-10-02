2c6109ebc2009f821501ccb810858e4a7aa8b64a
Diego <diegosantos4957@gmail.com>
2025-10-01 22:31:00 -0300

    feat: add reusable Input component for form fields

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,36 @@
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+
+type InputProps = {
+	label: string;
+	type?: string;
+	step?: string | number;
+	placeholder?: string;
+	error?: FieldError;
+	register: UseFormRegisterReturn;
+};
+
+export function Input(data: InputProps) {
+	const {
+		label,
+		type = "text",
+		placeholder,
+		error,
+		register,
+		step
+	} = data;
+	return (
+		<div className="w-full">
+			<label className="block text-sm font-medium text-gray-700">{label}</label>
+			<input
+				type={type}
+				placeholder={placeholder}
+				step={step}
+				{...register}
+				className={`text-gray-500 w-full mt-1 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500
+          ${error ? "border-red-500" : "border-gray-300"}
+        `}
+			/>
+			{error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
+		</div>
+	);
+}
