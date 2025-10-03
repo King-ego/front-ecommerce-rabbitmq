72d0d4237a0ae3622d3626beb90bd99330faf816
Diego <diegosantos4957@gmail.com>
2025-10-02 21:25:35 -0300

    feat: add error handling to Input fields in form

--- src/app/products/create/page.tsx
+++ src/app/products/create/page.tsx
@@ -46,18 +46,18 @@ export default function CadastroProduto() {
 				</div>
 
 				<form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
-					<Input label={"Nome do Produto"} register={{...register("name", {required: "Informe o nome do produto"})}} placeholder={"Digite o nome do produto"}/>
+					<Input label={"Nome do Produto"} register={{...register("name", {required: "Informe o nome do produto"})}} placeholder={"Digite o nome do produto"} error={errors.name}/>
 
 					<div className="grid grid-cols-2 gap-4">
 
-						<Input label={"Preço (R$)"} register={{...register("price", { required: "Informe o preço" })}} step={"0.01"} type={"number"} />
+						<Input label={"Preço (R$)"} register={{...register("price", { required: "Informe o preço" })}} step={"0.01"} type={"number"} error={errors.price} />
 
-						<Input label={"Quantidade em Estoque"} register={{...register("quantity_in_stock", { required: "Informe a quantidade" })}} type={"number"} />
+						<Input label={"Quantidade em Estoque"} register={{...register("quantity_in_stock", { required: "Informe a quantidade" })}} type={"number"} error={errors.quantity_in_stock}/>
 
 					</div>
 
 					{/* Categoria */}
-
+					
 
 					{/* Descrição */}
 					<div>
