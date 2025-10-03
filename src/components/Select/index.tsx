37b4bcc811edb649e0031053c1374565f0defdeb
Diego <diegosantos4957@gmail.com>
2025-10-02 21:23:19 -0300

    feat: enhance Select component to accept dynamic label and placeholder props

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,20 +1,35 @@
-export default function Select() {
+import {FieldError, UseFormRegisterReturn} from "react-hook-form";
+
+type InputProps = {
+	label: string;
+	placeholder?: string;
+	error?: FieldError;
+	register: UseFormRegisterReturn;
+};
+
+export default function Select(data: InputProps) {
+	const {
+		register,
+		error,
+		placeholder,
+		label
+	} = data;
 	return(
 		<div className={"w-full"}>
 			<label className="block text-sm font-medium text-gray-700">
-				Categoria
+				{label}
 			</label>
 			<select
-				{...register("category", { required: "Selecione uma categoria" })}
+				{...register}
 				className="w-full mt-1 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 text-gray-500"
 			>
-				<option value="" disabled>Selecione uma categoria</option>
+				<option value="" disabled>{placeholder}</option>
 				<option value="eletronicos">Eletrônicos</option>
 				<option value="roupas">Roupas</option>
 				<option value="moveis">Móveis</option>
 			</select>
-			{errors.category && (
-				<p className="text-red-500 text-sm mt-1">{errors.category.message}</p>
+			{error && (
+				<p className="text-red-500 text-sm mt-1">{error.message}</p>
 			)}
 		</div>
 	)
