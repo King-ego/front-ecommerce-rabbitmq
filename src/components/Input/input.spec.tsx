9cd98baffafd40ea99262fa80b50bc334c451771
Diego <diegosantos4957@gmail.com>
2025-10-05 11:04:38 -0300

    test: enhance Input component tests with additional scenarios and form submission handling

--- src/components/Input/input.spec.tsx
+++ src/components/Input/input.spec.tsx
@@ -1,5 +1,5 @@
 import {Input} from "@/components/Input/index";
-import {render, screen} from "@testing-library/react";
+import {render, screen, fireEvent} from "@testing-library/react";
 import {useForm} from "react-hook-form";
 
 type FormValues = {
@@ -10,10 +10,13 @@ function TestWrapper() {
 	const {
 		register,
 		formState: {errors},
+		handleSubmit
 	} = useForm<FormValues>();
 
+	const onSubmit = () => {}
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit(onSubmit)}>
 			<Input
 				label={"Test Input"}
 				register={register("testInput", {required: "This field is required"})}
@@ -31,9 +34,35 @@ describe("Input Component", () => {
 		jest.clearAllMocks();
 	})
 
-	it('should render input success', async () => {
+	it('should render input success with label', async () => {
 		render(<TestWrapper />)
 		expect(screen.getByTestId("test-input")).toBeInTheDocument();
 
+	});
+
+	it('should render placeholder', async () => {
+		render(<TestWrapper />)
+		expect(screen.getByPlaceholderText("Enter test input")).toBeInTheDocument();
+	});
+
+	it('should accept user input', async () => {
+		render(<TestWrapper/>);
+
+		const input = screen.getByTestId("test-input");
+
+		fireEvent.change(input, { target: { value: 'texto teste' } });
+
+		expect(input).toHaveValue('texto teste');
+
+	});
+
+	it('should display error message when fails', async () => {
+		render(<TestWrapper />);
+
+		const submitButton = screen.getByTestId("test-button");
+
+		fireEvent.click(submitButton);
+
+		expect(await screen.findByText("This field is required")).toBeInTheDocument();
 	});
 })
