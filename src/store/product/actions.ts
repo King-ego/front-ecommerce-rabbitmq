31ed5c9821264c5173e8106ee02ee5e60e7c0d26
Diego <diegosantos4957@gmail.com>
2025-10-07 23:28:26 -0300

    fix: remove unnecessary line breaks in product slice actions for improved readability

--- src/store/product/actions.ts
+++ src/store/product/actions.ts
@@ -3,14 +3,17 @@ import { ProductState } from "./type";
 
 export const createProductSlice: StateCreator<ProductState> = (set) => ({
 	products: [],
+
 	addProduct: (product) =>
 		set((state) => ({
 			products: [...state.products, product],
 		})),
+
 	deleteProduct: (productId) =>
 		set((state) => ({
 			products: state.products.filter((p) => p.id !== productId),
 		})),
+
 	setProducts: (products) => {
 		set({products})
 	},
