3a89fceb1bb974a2cd283b16daacec15f4d1a0c7
Diego <diegosantos4957@gmail.com>
2025-10-07 23:27:41 -0300

    fix: adjust indentation in input component for improved readability

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -31,8 +31,8 @@ export function Input(data: InputProps) {
 				data-testid={testId}
 				{...rest}
 				className={`text-gray-500 w-full mt-1 px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500
-          ${error ? "border-red-500" : "border-gray-300"}
-        `}
+          			${error ? "border-red-500" : "border-gray-300"}
+        		`}
 			/>
 			{error && <p className="text-red-500 text-sm mt-1">{error.message}</p>}
 		</div>
