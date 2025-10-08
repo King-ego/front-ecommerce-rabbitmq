28c6a42f23e1555801bcaec7b488142d89a8f5be
Diego <diegosantos4957@gmail.com>
2025-10-07 23:28:46 -0300

    fix: remove unnecessary line breaks in ProductState interface for improved readability

--- src/store/product/type.ts
+++ src/store/product/type.ts
@@ -2,7 +2,10 @@ import Product from "@/requests/interfaces/Product";
 
 export interface ProductState {
 	products: Product[];
+
 	addProduct: (product: Product) => void;
+
 	deleteProduct: (productId: string) => void;
+
 	setProducts: (products: Product[]) => void;
 }
