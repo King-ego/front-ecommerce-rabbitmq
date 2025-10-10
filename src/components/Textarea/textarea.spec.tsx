8fd54e8bb5292004a5e984040d332feb75d54826
Diego <diegosantos4957@gmail.com>
2025-10-09 23:43:13 -0300

    test: add unit tests for Textarea component

---
+++ src/components/Textarea/textarea.spec.tsx
@@ -0,0 +1,43 @@
+import Textarea from './index';
+
+import {render, screen, fireEvent} from "@testing-library/react";
+import {useForm} from "react-hook-form";
+
+type FormValues = {
+	testTextarea: string;
+};
+
+function TestWrapper() {
+	const {
+		register,
+		formState: {errors},
+		handleSubmit
+	} = useForm<FormValues>();
+
+	const onSubmit = () => {
+	}
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Textarea
+				label={"Test Textarea"}
+				placeholder="Enter test textarea"
+				register={register("testTextarea", {required: "This field is required"})}
+				error={errors.testTextarea}
+				testId="test-textarea"
+			/>
+			<button type="submit" data-testid="test-button">Submit</button>
+		</form>
+	);
+}
+
+describe("Textarea Component", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	})
+
+	it('should render textarea success with label', async () => {
+		render(<TestWrapper/>)
+		expect(screen.getByTestId("test-textarea")).toBeInTheDocument();
+	});
+});
