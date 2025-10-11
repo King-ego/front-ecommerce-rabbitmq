e39cbc6448e92743a6680ec6788639970f34d5ef
Diego <diegosantos4957@gmail.com>
2025-10-10 22:24:23 -0300

    test: mock navigation for SearchPage tests and update expected text

--- src/app/products/search/search.spec.tsx
+++ src/app/products/search/search.spec.tsx
@@ -1,6 +1,15 @@
 import { render } from '@testing-library/react';
 import SearchPage from "./page";
 
+jest.mock('next/navigation', () => ({
+	redirect: jest.fn(),
+	useRouter: jest.fn().mockReturnValue({
+		push: jest.fn(),
+		query: {},
+	}),
+	useSearchParams: jest.fn().mockReturnValue(new URLSearchParams()),
+}));
+
 describe('Search Page', () => {
 	beforeEach(() => {
 		jest.clearAllMocks();
@@ -8,6 +17,6 @@ describe('Search Page', () => {
 
 	it('should render search page', async () => {
 		const { findByText } = render(<SearchPage />);
-		expect(await findByText("Resultados para: test")).toBeInTheDocument();
+		expect(await findByText("Fitros de Produtos")).toBeInTheDocument();
 	});
 })
