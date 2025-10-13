aec41573b5a7940bee6b87c19ae3c73242c960bf
Diego <diegosantos4957@gmail.com>
2025-10-12 21:49:18 -0300

    test: remove unused fireEvent import in textarea tests

--- src/components/Textarea/textarea.spec.tsx
+++ src/components/Textarea/textarea.spec.tsx
@@ -1,6 +1,6 @@
 import Textarea from './index';
 
-import {render, screen, fireEvent} from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import {useForm} from "react-hook-form";
 
 type FormValues = {
