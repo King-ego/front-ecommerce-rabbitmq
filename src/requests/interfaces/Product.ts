431bd1827eb3633fdd08f0b3c108a9b7d1c5f7f1
Diego <diegosantos4957@gmail.com>
2025-10-13 23:07:07 -0300

    refactor: format Product interface for improved readability

--- src/requests/interfaces/Product.ts
+++ src/requests/interfaces/Product.ts
@@ -1,10 +1,17 @@
 export default interface Product {
     id: string;
+
     name: string;
-    price: number;
-    quantity_in_stock: number;
-    description: string;
-    category: string;
-    image_url: string;
+
+	price: number;
+
+	quantity_in_stock: number;
+
+	description: string;
+
+	category: string;
+
+	image_url: string;
+
 	slug: string;
 }
