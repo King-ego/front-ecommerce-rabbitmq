cd01c24e6d948058dc48c2792eecf2184adbc99b
Diego <diegosantos4957@gmail.com>
2025-10-16 00:19:04 -0300

    feat: refactor page component to use server-side rendering and integrate Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,24 @@
-'use client';
+'use server';
 
-import {Suspense, useEffect, useRef, useState} from "react";
 import {redirect} from "next/navigation";
 import clsx from "clsx";
 import {useProductStore} from "@/store";
 import {ProductHttpService} from "@/requests/http/services/ProductHttpService";
+import Home from "@/app/Home";
 
-export default function Home() {
-	const [loading, setLoading] = useState(true);
-	const { products, setProducts } = useProductStore();
-	const ref = useRef<HTMLInputElement | null>(null);
+export default async function ProductsPage() {
+	const initialProducts = await ProductHttpService.getProducts();
 
-	useEffect(()=> {
-		ProductHttpService.getProducts()
-			.then(resp=> {
-				setProducts(resp)
-			})
-			.finally(()=>setLoading(state => !state))
-	},[setProducts])
-
-	const handleSearch = () => {
-		if (ref.current?.value) {
-			redirect(`/products/search?q=${ref.current.value || ''}`);
-		}
-	}
-
-	const formatPrice = ((price: number) => {
-		const value = Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"}).format(price)
-
-		const [integer, decimal] = value.split(',');
-		return (
-			<span>
-				<span className="text-2xl font-bold text-gray-900">{integer}</span>
-				{decimal && <span className="text-sm text-gray-500">,{decimal}</span>}
-			</span>
-		)
-	})
-
+/*
 	if(!loading && !products.length) {
 		return (
 			<div className="flex items-center justify-center min-h-screen bg-gray-100">
 				<div className="text-gray-500">Nenhum Produto Encontrado</div>
 			</div>
 		)
-	}
+	}*/
 
 	return (
-		<></>
+		<Home initialProducts={initialProducts} />
 	);
 }
