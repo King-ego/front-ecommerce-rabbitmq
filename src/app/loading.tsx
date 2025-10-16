cf355a334723d0aa5b84787dd31b700a0c210a1f
Diego <diegosantos4957@gmail.com>
2025-10-16 00:30:58 -0300

    feat: add loading component to enhance user experience during data fetching

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-[#171717]">
+			<div className="text-gray-400 text-lg animate-pulse">
+				Carregando produtos...
+			</div>
+		</div>
+	);
+}
