56ca4db2bff96f3bca76859713090eaf4f96f666
Diego <diegosantos4957@gmail.com>
2025-10-16 00:52:23 -0300

    refactor: remove commented-out code for cleaner page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,6 @@ import Home from "@/app/Home";
 export default async function ProductsPage() {
 	const initialProducts = await ProductHttpService.getProducts();
 
-/*
-	if(!loading && !products.length) {
-		return (
-			<div className="flex items-center justify-center min-h-screen bg-gray-100">
-				<div className="text-gray-500">Nenhum Produto Encontrado</div>
-			</div>
-		)
-	}*/
-
 	return (
 		<Home initialProducts={initialProducts} />
 	);
