d6e3487fcc08189c0b8cabc48bdbe5819a785e95
Diego <diegosantos4957@gmail.com>
2025-11-03 21:12:30 -0300

    fix: add missing newline in HomePage.tsx for consistency

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -64,13 +64,14 @@ export default function HomePage(props: HomeProps) {
 								onClick={() => handleSearch()}
 								data-testid="search_button"
 							>
-								
+
 								<svg className="w-5 h-5" fill="none" stroke="#171717" viewBox="0 0 24 24">
 									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
 										  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
 								</svg>
 
 							</button>
+							
 						</div>
 
 						<input
